16280e375596f666c98c700f9faff6a893a208bb
Yezizheng <yezi@gwmail.gwu.edu>
2020-06-19 16:36:35 +0800

    build, feature, update-deps
    services:
        - Added docker:start and start:cron scripts
        - Added dependencies:
            "axios": "^0.19.2",
            "node-cron": "^2.0.3"
        - Added src/cron.js to include seed for Manga Eden
        - Added .env.example
        - Updated db/modles/Manga to include lastUpdated property

--- services/src/index.js
+++ services/src/index.js
@@ -1,21 +1,3 @@
 import "dotenv/config";
 
-import "./db/connection";
 import "./server";  
-
-import Manga from "./db/models/Manga";
-
-const manga = new Manga({
-    "_id" : "5db37427719a168daca60ff5",
-    "alias" : "haunted",
-    "categories" : [],
-    "hits" : 0,
-    "image" : null,
-    "status" : 1,
-    "title" : "HAUNTED (ホーンテッド)"
-});
-
-manga.save(function (err, manga) {
-    if (err) return console.error(err);
-    console.log("saved");
-})
